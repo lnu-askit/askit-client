14d48f2425385b4ec7b335e43bcb2672fdb8818b
Denni Wernersson <dw222fy@student.lnu.se>
2023-03-17 18:39:17 +0100

    Add connection to openAI

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,22 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import type { NextApiRequest, NextApiResponse } from "next";
 import type {
-  ChatCompletionRequestMessage,
+  CreateChatCompletionRequest,
+  ChatCompletionRequestMessage
 } from "openai";
 
 interface SubscribeRequest extends NextApiRequest {
   body: ChatCompletionRequestMessage[];
 }
 
-export default function handler(
+export default async function handler(
   req: SubscribeRequest,
   res: NextApiResponse
 ) {
 
-  const reqMessages: ChatCompletionRequestMessage[] = req.body;
+  const reqMessages: ChatCompletionRequestMessage[] = req.body.map((message) => ({
+    role: message.role,
+    content: message.content
+  }));
 
-  console.log(reqMessages)
-  setTimeout(() => {
-    res.status(200).json({user: 'assistant', content: 'what?'})
-    return
-  }, 2000)
+  const prompt = 'You are a chatbot. Behave accordingly.'
+  const messages: ChatCompletionRequestMessage[] = [
+    { role: 'system', content: prompt },
+    ...reqMessages
+  ]
+
+  const chatRequestOpts: CreateChatCompletionRequest = {
+    model: 'gpt-3.5-turbo',
+    messages,
+    temperature: 0.1
+  }
+
+  console.log(messages)
+  console.log(chatRequestOpts)
+
+  const chatResponse = await fetch('https://api.openai.com/v1/chat/completions', {
+			headers: {
+				Authorization: `Bearer ${OPENAI_KEY}`,
+				'Content-Type': 'application/json'
+			},
+			method: 'POST',
+			body: JSON.stringify(chatRequestOpts)
+		})
+
+  const answer = await chatResponse.json()
+
+  console.log(answer)
+  res.status(200).json(answer.choices[0].message)
 }
