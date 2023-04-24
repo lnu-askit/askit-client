b1005f7b6ec0058e2e07d9192f23939ebeb709d7
Denni Wernersson <dw222fy@student.lnu.se>
2023-04-24 15:18:22 +0200

    Add context retrieval to admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,89 +2,102 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import Head from 'next/head';
-import { useState } from 'react';
-import { ChatMessage } from '~/components/ChatMessage';
-import { MessageInput } from '~/components/MessageInput';
-import { AdminPageLayout } from '~/components/adminLayout';
-import { dummySystem, dummyContext } from 'utils/dummyContent';
+import Head from 'next/head'
+import { useRef, useState } from 'react'
+import { ChatMessage } from '~/components/ChatMessage'
+import { MessageInput } from '~/components/MessageInput'
+import { AdminPageLayout } from '~/components/adminLayout'
+import { dummySystem } from 'utils/dummyContent'
 
 export type ChatMessageProps = {
-  id: string;
-  role: string;
-  content: string;
-  model?: string;
-};
+  id: string
+  role: string
+  content: string
+  model?: string
+}
 
 export default function Home() {
-  const [messages, setMessages] = useState<ChatMessageProps[]>([]);
-  const [system, setSystem] = useState(dummySystem);
-  const [context, setContext] = useState('');
+  const [messages, setMessages] = useState<ChatMessageProps[]>([])
+  const [system, setSystem] = useState(dummySystem)
+  const context = useRef('')
 
-  const model = process.env.NEXT_PUBLIC_GPT_MODEL;
+  const model = process.env.NEXT_PUBLIC_GPT_MODEL
 
-  function handleNewContext() {
-    setContext(dummyContext);
+  async function handleNewContext(content: string) {
+    const response = await fetch('/api/retrieval', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: content,
+      }),
+    })
+
+    const json = (await response.json()) as string
+    context.current = json
   }
 
   async function handleNewMessage(content: string) {
-    const role = 'user';
+    const role = 'user'
     const sendMessages = [
       ...messages,
       { id: Math.random().toString(36).substring(2, 15), role, content },
-    ];
-    setMessages(sendMessages);
+    ]
+    setMessages(sendMessages)
 
-    if (context === '') {
-      handleNewContext();
+    if (context.current === '') {
+      await handleNewContext(content)
     }
 
     const response = await fetch('/api/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ system: system, context: context, chatMessages: sendMessages }),
-    });
+      body: JSON.stringify({
+        system: system,
+        context: context.current,
+        chatMessages: sendMessages,
+      }),
+    })
 
     if (response.body) {
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder('utf-8');
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder('utf-8')
 
       const newBotMessage: ChatMessageProps = {
         id: Math.random().toString(36).substring(2, 15),
         role: 'assistant',
         content: '',
-      };
+      }
 
       while (true) {
-        const { value, done } = await reader.read();
-        if (done) break;
+        const { value, done } = await reader.read()
+        if (done) break
 
-        const messageText = decoder.decode(value);
-        newBotMessage.content += messageText;
+        const messageText = decoder.decode(value)
+        newBotMessage.content += messageText
 
         // there may be a JSON object at the beginning of the message, which contains the model name (streaming workaround)
         if (!newBotMessage.model && newBotMessage.content.startsWith('{')) {
-          const endOfJson = newBotMessage.content.indexOf('}');
+          const endOfJson = newBotMessage.content.indexOf('}')
           if (endOfJson > 0) {
-            const json = newBotMessage.content.substring(0, endOfJson + 1);
+            const json = newBotMessage.content.substring(0, endOfJson + 1)
             try {
-              const parsed = JSON.parse(json);
-              newBotMessage.model = parsed.model;
-              newBotMessage.content = newBotMessage.content.substring(endOfJson + 1);
+              const parsed = JSON.parse(json)
+              newBotMessage.model = parsed.model
+              newBotMessage.content = newBotMessage.content.substring(endOfJson + 1)
             } catch (e) {
               // error parsing JSON, ignore
-              console.log(e);
+              console.log(e)
             }
           }
         }
 
         setMessages((list) => {
           // find the message to update with new text tokens.
-          const message = list.find((message) => message.id === newBotMessage.id);
+          const message = list.find((message) => message.id === newBotMessage.id)
 
           // update the message with the new text content, re-render the message-list anyway if the message couldn't be found.
-          return !message ? [...list, newBotMessage] : [...list];
-        });
+          return !message ? [...list, newBotMessage] : [...list]
+        })
       }
     }
   }
@@ -110,7 +123,7 @@ export default function Home() {
               placeholder="Set system message..."
               value={system}
               onChange={(e) => {
-                setSystem(e.target.value);
+                setSystem(e.target.value)
               }}
               required
             />
@@ -118,12 +131,13 @@ export default function Home() {
           <div className="mr-2 flex h-full w-full flex-col overflow-y-hidden rounded-sm border border-slate-800 bg-slate-700 text-slate-200">
             <div className="border-b border-slate-800 bg-slate-600 p-2 text-center">Context</div>
             <textarea
+              disabled
               className="normal-whitespace h-full w-full resize-none overflow-y-auto break-words bg-transparent p-4 text-sm text-slate-200 outline-none scrollbar-thin scrollbar-thumb-slate-500 scrollbar-thumb-rounded-lg"
               name="context"
-              placeholder="Set context..."
-              value={context}
+              placeholder="No context loaded..."
+              value={context.current}
               onChange={(e) => {
-                setContext(e.target.value);
+                context.current = e.target.value
               }}
               required
             />
@@ -146,5 +160,5 @@ export default function Home() {
         </div>
       </AdminPageLayout>
     </>
-  );
+  )
 }
