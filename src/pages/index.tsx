7538975dd2f7c446d08806f384ec57de23ee5c1d
Denni Wernersson <dw222fy@student.lnu.se>
2023-04-24 15:18:33 +0200

    Add context retrieval to main chat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,48 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
-import Head from 'next/head';
-import { useState } from 'react';
-import { ChatMessage } from '~/components/ChatMessage';
-import { MessageInput } from '~/components/MessageInput';
-import { PageLayout } from '~/components/layout';
-import { dummyContext, dummyInstructions, dummySystem } from 'utils/dummyContent';
-import { InstructionsCard } from '~/components/InstructionsCard';
+import Head from 'next/head'
+import { useRef, useState } from 'react'
+import { ChatMessage } from '~/components/ChatMessage'
+import { MessageInput } from '~/components/MessageInput'
+import { PageLayout } from '~/components/layout'
+import { dummyInstructions, dummySystem } from 'utils/dummyContent'
+import { InstructionsCard } from '~/components/InstructionsCard'
 
 export type ChatMessageProps = {
-  id: string;
-  role: string;
-  content: string;
-  model?: string;
-};
+  id: string
+  role: string
+  content: string
+  model?: string
+}
 
 export default function Home() {
-  const [context, setContext] = useState('');
-  const [messages, setMessages] = useState<ChatMessageProps[]>([]);
+  const context = useRef('')
+  const [messages, setMessages] = useState<ChatMessageProps[]>([])
 
-  function handleNewContext() {
-    setContext(dummyContext);
+  async function handleNewContext(content: string) {
+    const response = await fetch('/api/retrieval', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        query: content,
+      }),
+    })
+
+    const json = await response.json()
+    context.current = json
   }
 
   async function handleNewMessage(content: string) {
-    const role = 'user';
+    const role = 'user'
     const sendMessages = [
       ...messages,
       { id: Math.random().toString(36).substring(2, 15), role, content },
-    ];
-    setMessages(sendMessages);
+    ]
+    setMessages(sendMessages)
 
-    if (context === '') {
-      handleNewContext();
+    if (context.current === '') {
+      await handleNewContext(content)
     }
 
     const response = await fetch('/api/chat', {
@@ -42,51 +51,51 @@ export default function Home() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         system: dummySystem,
-        context: context,
+        context: context.current,
         chatMessages: sendMessages,
       }),
-    });
+    })
 
     if (response.body) {
-      const reader = response.body.getReader();
-      const decoder = new TextDecoder('utf-8');
+      const reader = response.body.getReader()
+      const decoder = new TextDecoder('utf-8')
 
       const newBotMessage: ChatMessageProps = {
         id: Math.random().toString(36).substring(2, 15),
         role: 'assistant',
         content: '',
-      };
+      }
 
       while (true) {
-        const { value, done } = await reader.read();
-        if (done) break;
+        const { value, done } = await reader.read()
+        if (done) break
 
-        const messageText = decoder.decode(value);
-        newBotMessage.content += messageText;
+        const messageText = decoder.decode(value)
+        newBotMessage.content += messageText
 
         // there may be a JSON object at the beginning of the message, which contains the model name (streaming workaround)
         if (!newBotMessage.model && newBotMessage.content.startsWith('{')) {
-          const endOfJson = newBotMessage.content.indexOf('}');
+          const endOfJson = newBotMessage.content.indexOf('}')
           if (endOfJson > 0) {
-            const json = newBotMessage.content.substring(0, endOfJson + 1);
+            const json = newBotMessage.content.substring(0, endOfJson + 1)
             try {
-              const parsed = JSON.parse(json);
-              newBotMessage.model = parsed.model;
-              newBotMessage.content = newBotMessage.content.substring(endOfJson + 1);
+              const parsed = JSON.parse(json)
+              newBotMessage.model = parsed.model
+              newBotMessage.content = newBotMessage.content.substring(endOfJson + 1)
             } catch (e) {
               // error parsing JSON, ignore
-              console.log(e);
+              console.log(e)
             }
           }
         }
 
         setMessages((list) => {
           // find the message to update with new text tokens.
-          const message = list.find((message) => message.id === newBotMessage.id);
+          const message = list.find((message) => message.id === newBotMessage.id)
 
           // update the message with the new text content, re-render the message-list anyway if the message couldn't be found.
-          return !message ? [...list, newBotMessage] : [...list];
-        });
+          return !message ? [...list, newBotMessage] : [...list]
+        })
       }
     }
   }
@@ -101,7 +110,7 @@ export default function Home() {
 
       <PageLayout>
         <div className="relative h-full bg-slate-700">
-          {context === '' && <InstructionsCard message={dummyInstructions()} />}
+          {context.current === '' && <InstructionsCard message={dummyInstructions()} />}
 
           <div className="flex h-full flex-col overflow-y-auto scrollbar-thin scrollbar-track-transparent scrollbar-thumb-slate-500 scrollbar-thumb-rounded-lg">
             <div className="w-full flex-nowrap border-b border-slate-800 bg-slate-600 p-2 text-center text-slate-200">
@@ -119,5 +128,5 @@ export default function Home() {
         </div>
       </PageLayout>
     </>
-  );
+  )
 }
